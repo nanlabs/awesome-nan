8da0f14e345470c527d6296038547263bd8b9f4b
ulises-jeremias <ulisescf.24@gmail.com>
2023-07-18 02:54:31 -0300

    Updated script

--- tools/readme-generator/main.ts
+++ tools/readme-generator/main.ts
@@ -1,36 +1,49 @@
 #!/usr/bin/env -S deno run --allow-read --allow-write --allow-run
 
-import { groupExamplesByTags, readExamplesFromJsonPaths } from "./transform.ts";
-import { generateContent, generateToc } from "./generator.ts";
+import { groupExamplesByTags, readExamplesFromJsonPaths } from "./lib/transform.ts";
+import { generateContent, generateToc } from "./lib/generator.ts";
 
 async function main() {
   if (Deno.args.length < 2) {
     console.log(
       "Usage: generate.ts README.md.tmpl examples1.json [examples2.json ...]",
+      "\n",
+      "README.md.tmpl: The template file to use to generate the README.md file.",
+      "\n",
+      "examples1.json, examples2.json, ...: The JSON files containing the examples.",
+      "\n",
+      "Options:",
+      "\n",
+      "--json: Generate a JSON file instead of a README.md file.",
+    );
+    console.log(
+      "Example: generate.ts README.md.tmpl examples1.json examples2.json",
+    );
+    console.log(
+      "Example: generate.ts --json examples1.json examples2.json > examples.json",
     );
     Deno.exit(1);
   }
 
-  console.log("Reading examples.json...");
+  const shouldGenerateJson = Deno.args.includes("--json");
 
   const examples = await readExamplesFromJsonPaths([...Deno.args.slice(1)]);
   const examplesByTags = await groupExamplesByTags(examples);
 
+  if (shouldGenerateJson) {
+    console.log(JSON.stringify(examplesByTags, null, 2));
+    return;
+  }
+
   const content = generateContent(examplesByTags);
   const toc = generateToc(examplesByTags);
-
-  console.log("Reading README.md.tmpl...");
 
   const readmeTmplPath = Deno.args[0];
   const readmeTmpl = await Deno.readTextFile(readmeTmplPath);
 
-  console.log("Generating README.md...");
-
   const readme = readmeTmpl
     .replace("{{toc}}", toc)
     .replace("{{content}}", content);
-
-  console.log("Writing README.md...");
 
   await Deno.writeTextFile("README.md", readme);
 }
